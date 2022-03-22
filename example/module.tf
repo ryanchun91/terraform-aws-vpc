0470564ced34451a10319476e5b9cc774869a60b
Jiyoung Chun <95110185+ryanchun91@users.noreply.github.com>
2022-03-21 19:26:44 -0500

    subnets added

--- example/module.tf
+++ example/module.tf
@@ -1,6 +1,7 @@
 module "vpc" {
     source = "../"
     cidr_block = "10.0.0.0/16"
+    private_subnets_cidr = ["10.0.1.0/24"]
     tags = {
         Name = "Dev"
     }
--- variable.tf
+++ variable.tf
@@ -2,3 +2,7 @@ variable "cidr_block" {}
 variable "tags" {
     type = map
 }
+
+variable "private_subnets_cidr" {
+    type = list
+}
